2e50bd80828f113d48656c3c4d2a799a86043521
Pasha <78595276+PolyakPavlo@users.noreply.github.com>
2021-03-31 14:10:19 +0300

    Update sql_orm.py

--- hw12/python/sql_orm.py
+++ hw12/python/sql_orm.py
@@ -4,13 +4,6 @@ import sys
 import datetime
 import pandas as pd
 import re
-ssh_client =paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(hostname='207.244.229.74',username='testuser',password='*,<R#!$(2udw{Zgz')
-ftp_client=ssh_client.open_sftp()
-ftp_client.get('/opt/testuser/logfile.log','logfile.log')
-ftp_client.close()
-time.sleep(10)
 
 c=1
 logs_entries=[]
