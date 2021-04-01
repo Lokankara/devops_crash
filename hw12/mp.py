b5404fbb3a9d72021079a3e285ae140b92a423d9
Pasha <78595276+PolyakPavlo@users.noreply.github.com>
2021-04-01 16:49:41 +0300

    Update mp.py

--- hw12/mp.py
+++ hw12/mp.py
@@ -1,4 +1,4 @@
-
+import paramiko
 ssh_client =paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname='207.244.229.74',username='testuser',password='*,<R#!$(2udw{Zgz')
