900c3130fae45767d43d463dee94c7204e488c9d
Pasha <78595276+PolyakPavlo@users.noreply.github.com>
2021-04-01 15:00:21 +0300

    Create mp.py

---
+++ hw12/mp.py
@@ -0,0 +1,7 @@
+
+ssh_client =paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(hostname='207.244.229.74',username='testuser',password='*,<R#!$(2udw{Zgz')
+ftp_client=ssh_client.open_sftp()
+ftp_client.put('access.db','/home/testuser/pavlo_polyak/access.db')
+ftp_client.close()
