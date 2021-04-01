36e61769b9ba75d80b3e3b76c0a2d4a34b409348
Pasha <78595276+PolyakPavlo@users.noreply.github.com>
2021-04-01 13:09:20 +0300

    Update db.py

--- hw12/python/db.py
+++ hw12/python/db.py
@@ -49,19 +49,6 @@ session.add(log)
 session.commit()
 session.close()
 
-logs_entries=[]
-try: file = open(sys.argv[1], 'r')
-except:  file = open('logfile.log', 'r')
-
-for lines in file:
-    line = re.compile(r'^((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+(\S+)\s+(sshd)\S+:\s+(.*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*)$').search(lines)
-    if line:
-        print(line)
-        datetime_str = line.group(1) + " " +str(datetime.datetime.now().year)
-        datetime_obj = datetime.datetime.strptime(datetime_str,'%b %d %H:%M:%S %Y')
-        logs_entries.append({"hostname": line.group(3),"ip_address": line.group(6), "date_time": datetime_obj,"message": line.group(5)})
-file.close()
-
 df = pd.DataFrame(logs_entries)
 #df["hostname"].mask(df["hostname"].duplicated(),inplace=True)
 #df["ip_address"].mask(df["ip_address"].duplicated(),inplace=True)
