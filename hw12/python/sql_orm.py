489b3d33497d0bea95cc0ba3a219d0f15c893b10
Pasha <78595276+PolyakPavlo@users.noreply.github.com>
2021-04-01 12:01:36 +0300

    Update sql_orm.py

--- hw12/python/sql_orm.py
+++ hw12/python/sql_orm.py
@@ -17,7 +17,7 @@ ssh_client.connect(hostname='207.244.229.74',username='testuser',password='*,<R#
 ftp_client=ssh_client.open_sftp()
 ftp_client.get('/opt/testuser/logfile.log','logfile.log')
 ftp_client.close()
-time.sleep(10)
+time.sleep(100)
 
 err=[]
 logs_entries = []
@@ -80,5 +80,6 @@ ssh_client =paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname='207.244.229.74',username='testuser',password='*,<R#!$(2udw{Zgz')
 ftp_client=ssh_client.open_sftp()
+ftp_client.put('access.db','/home/testuser/pavlo_polyak/access.db')
 ftp_client.put('access.xlsx','/home/testuser/pavlo_polyak/access_logs.xlsx')
 ftp_client.close()
